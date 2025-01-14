9e41cc3d98930cd91af1df782c2ef6fc92e3e120
Rodney Chery <rchery7@wgu.edu>
2025-01-14 07:43:23 -0500

    -Bootstrap 5 framework review -Understood the layout file -Reviewed the static files, libraries, and references -Changed the applications layout

--- LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
+++ LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0528333a3e733ae933ba3d5f38fde36444c39eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef5784cad0d4acd71f6cd50197e127b06c1248a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
