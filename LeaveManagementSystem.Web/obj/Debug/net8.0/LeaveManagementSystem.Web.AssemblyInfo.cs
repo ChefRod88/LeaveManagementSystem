3d4e60ef5360c30455d5f512fa024a9db8fd7871
Rodney Chery <rchery7@wgu.edu>
2025-01-17 04:21:50 -0500

    my computer is down

--- LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
+++ LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e41cc3d98930cd91af1df782c2ef6fc92e3e120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c8f23972a7acafe47f52bd430fd8871dba9462")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
